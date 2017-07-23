5b2fae8a4183e11b43cf19a916be9dcb848a34de
Heidi Kasemir <heidi.kasemir@gmail.com>
2017-07-22 21:02:31 -0700

    use pixabay to make it work again, a little bit of touch ups

--- js/app.js
+++ js/app.js
@@ -1,57 +1,56 @@
-'use strict';
+/* globals ko google */
 
 // basic data that will be loaded up on the map
 // 5 locations in Boulder, CO that I liked in college
-var locs = [{
-    address: '3240 Prairie Avenue, Boulder, CO 80301',
+const locs = [{
     title: 'The Spot Bouldering Gym',
-    latlng: {lat: 40.0215272, lng: -105.2505871}
+    latlng: {lat: 40.0215272, lng: -105.2505871},
+    image: 'rock climbing'
   },
   {
-    address: '825 Walnut Street, Boulder CO, 80302',
     title: 'Sherpa\'s Restaurant',
-    latlng: {lat: 40.0161644, lng: -105.2844167}
+    latlng: {lat: 40.0161644, lng: -105.2844167},
+    image: 'sherpa'
   },
   {
-    address: '1117 Pearl Street, Boulder, CO 80302',
     title: 'Hapa Sushi',
-    latlng: {lat: 40.018009, lng: -105.2505871}
+    latlng: {lat: 40.018009, lng: -105.2505871},
+    image: 'sushi'
   },
   {
-    address: '40.014290, -105.282472',
     title: 'Boulder Creek',
-    latlng: {lat: 40.0145327, lng: -105.2821675}
+    latlng: {lat: 40.0145327, lng: -105.2821675},
+    image: 'boulder creek'
   },
   {
-    address: '1111 Engineering Dr, Boulder, CO 80309',
     title: 'The CU Engineering Center',
-    latlng: {lat: 40.0071656, lng: -105.2627072}
+    latlng: {lat: 40.0071656, lng: -105.2627072},
+    image: 'engineering'
 }];
 
-var Location = function(data) {
-  this.address = data.address;
+function Location(data) {
   this.title = data.title;
   this.latlng = data.latlng;
   // markers are assigned later
   this.marker = null;
   this.selected = ko.observable(false);
   this.filtered = ko.observable(false);
-};
+  this.image = data.image;
+}
 
-var MapViewModel = function() {
-  var self = this;
-  self.map = null;
-  self.center = {lat: 40.02, lng: -105.27};
-  self.city = 'Boulder';
-  self.locations = ko.observableArray([]);
+function MapViewModel() {
+  this.map = null;
+  this.center = {lat: 40.02, lng: -105.27};
+  this.city = 'Boulder';
+  this.locations = ko.observableArray([]);
 
   locs.forEach((loc) => {
-    self.locations().push( new Location(loc));
+    this.locations().push(new Location(loc));
   });
 
-  self.addLocationMarkers = () => {
-    self.locations().forEach((loc) => {
-      loc.map = self.map;
+  this.addLocationMarkers = () => {
+    this.locations().forEach((loc) => {
+      loc.map = this.map;
 
       loc.marker = new google.maps.Marker({
         position: loc.latlng,
@@ -60,108 +59,65 @@ var MapViewModel = function() {
       });
       loc.marker.setMap(loc.map);
 
-      loc.marker.addListener('click', function() {
-        self.selectPlace(loc);
+      loc.marker.addListener('click', () => {
+        this.selectPlace(loc);
       });
 
-      // Then get Yelp data - this calls a build infowindow function as a success callback
-      self.fetchYelp(loc);
+      this.fetchImage(loc);
     });
   };
 
-  self.fetchYelp = (loc) => {
-    // From this very helpful gist : https://gist.github.com/kennygfunk/c24c8a2ea71c9ce7f4fc
-    var auth = {
-      consumerKey : '4NSfQz0B0AcatVl7p2CVQA',
-      consumerSecret : 'oGXlKX8vymwdBBx6khM8kij6G-U',
-      accessToken : 'QTEdTYBzg1dlfzUstVI6dFk4XJs-5A1M',
-      // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
-      // You wouldn't actually want to expose your access token secret like this in a real application.
-      accessTokenSecret : 'bku8a5nlw5zSPs_sUdeNlF3ou8M',
-      serviceProvider : {
-        signatureMethod : 'HMAC-SHA1'
-      }
-    };
-
-    var accessor = {
-      consumerSecret : auth.consumerSecret,
-      tokenSecret : auth.accessTokenSecret
-    };
-
-    var parameters = [];
-    parameters.push(['term', loc.title]);
-    parameters.push(['location', self.city]);
-    parameters.push(['callback', 'cb']);
-    parameters.push(['oauth_consumer_key', auth.consumerKey]);
-    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
-    parameters.push(['oauth_token', auth.accessToken]);
-    parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
-
-    var message = {
-      'action' : 'http://api.yelp.com/v2/search',
-      'method' : 'GET',
-      'parameters' : parameters
-    };
-    OAuth.setTimestampAndNonce(message);
-    OAuth.SignatureMethod.sign(message, accessor);
-
-    var parameterMap = OAuth.getParameterMap(message.parameters);
-    parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
-
-    $.ajax({
-      'url' : message.action,
-      'data' : parameterMap,
-      'cache' : true,
-      'dataType' : 'jsonp',
-      // using timeout to force error handling - may need to tweak this for mobile use.
-      'timeout' : 2000,
-      'success' : function(data) {
-        self.buildInfoWindow(data, loc);
-    },
-      'error' : function(jqXHR, textStatus, errorThrown) {
-        self.buildInfoWindow(textStatus, loc);
-      }
+  this.fetchImage = (loc) => {
+    fetch(`https://pixabay.com/api/?key=1553410-5d96ab654addbe62ddd7d6b2a&q=${encodeURIComponent(loc.image)}`).then(res => {
+      return res.json();
+    }).then((json) => {
+      this.buildInfoWindow(json, loc);
+    }).catch(() => {
+      this.buildInfoWindow('error', loc);
     });
   };
 
-  self.buildInfoWindow = (data, loc) => {
+  this.buildInfoWindow = (data, loc) => {
     // check to see if data indicates there was an error loading.
     if (typeof data == 'string'){
       loc.infowindow = new google.maps.InfoWindow({
-        content: 'Sorry, Yelp data about ' + loc.title.toLowerCase() +
-        ' could not be loaded'
+        content: `Sorry, an image for ${loc.title} could not be loaded`
       });
 
-      loc.infowindow.addListener('closeclick', function() {
-        self.selectPlace(loc);
+      loc.infowindow.addListener('closeclick', () => {
+        this.selectPlace(loc);
       });
       return;
     }
+    const imageUrl = data.hits[0].webformatURL;
 
     // if there were no errors:
-    var infoHtml = '<div class="info-window"><h2>' +
-        loc.title + '</h2><img src="' + data.businesses[0].snippet_image_url +
-        '"><p>' + data.businesses[0].snippet_text +
-        '<a href="' + data.businesses[0].url + '"> -> Read More</a></p></div>';
+    const infoHtml = `
+      <div class="info-window">
+        <h2>${loc.title}</h2>
+        <img src="${imageUrl}">
+        <span>image courtesy of Pixabay</span>
+      </div>
+    `;
 
     loc.infowindow = new google.maps.InfoWindow({
       content: infoHtml
     });
 
-    loc.infowindow.addListener('closeclick', function() {
-      self.selectPlace(loc);
+    loc.infowindow.addListener('closeclick', () => {
+      this.selectPlace(loc);
     });
 
   };
 
-  self.selectPlace = (loc) => {
+  this.selectPlace = (loc) => {
   // first, deselect other places
-    for (var i in self.locations()){
-      if (self.locations()[i] != loc &&
-          self.locations()[i].selected) {
-        self.locations()[i].selected(false);
-        self.locations()[i].infowindow.close();
-        self.locations()[i].marker.setAnimation(null);
+    for (let i in this.locations()){
+      if (this.locations()[i] != loc &&
+          this.locations()[i].selected) {
+        this.locations()[i].selected(false);
+        this.locations()[i].infowindow.close();
+        this.locations()[i].marker.setAnimation(null);
       }
     }
     if (loc.selected()) {
@@ -175,26 +131,26 @@ var MapViewModel = function() {
     }
   };
 
-  self.query = ko.observable('');
+  this.query = ko.observable('');
 
-  self.filter = (value) => {
+  this.filter = (value) => {
     // modified from this helpful post: http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
 
     // if they match search terms, make them visible
 
-    for (var i in locs) {
+    for (let i in locs) {
       if (locs[i].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-        self.locations()[i].filtered(false);
-        self.locations()[i].marker.setMap(self.map);
+        this.locations()[i].filtered(false);
+        this.locations()[i].marker.setMap(this.map);
       } else {
-        self.locations()[i].filtered(true);
-        self.locations()[i].marker.setMap(null);
+        this.locations()[i].filtered(true);
+        this.locations()[i].marker.setMap(null);
       }
     }
   };
-};
+}
 
-var MapView = new MapViewModel();
+const MapView = new MapViewModel();
 MapView.query.subscribe(MapView.filter);
 ko.applyBindings(MapView);
 
